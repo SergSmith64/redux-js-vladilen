1188b6e4d619edc843bbf60b76e55693b4591197
SergSmith64 <sergsmith2008@yandex.ru>
2021-01-31 09:50:03 +0400

    feat: add EventListeners to buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './styles.css'
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+
+let state = 0;
+
+// ___ оберну в функцию и вызову ее ниже ___
+function render() {
+    // ___ присваиваем счетчику значение ___
+    counter.textContent = state.toString();
+
+    // console.log("__счетчик__ = ", state);
+}
+
+addBtn.addEventListener('click', () => {
+    state++;
+    render();
+});
+
+subBtn.addEventListener('click', () => {
+    state--;
+    render();
+});
+
+render();
