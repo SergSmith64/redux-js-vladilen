69f025c920a2b1798727c947fa4ee7a3b2b48b91
SergSmith64 <sergsmith2008@yandex.ru>
2021-02-02 10:39:32 +0400

    feat: add Async

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles.css'
 const counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
 const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
 
 let state = 0;
 
@@ -23,5 +24,12 @@ subBtn.addEventListener('click', () => {
     state--;
     render();
 });
+
+asyncBtn.addEventListener('click', () => {
+    setTimeout ( () => {
+        state++;
+        render();
+    }, 2000)
+})
 
 render();
