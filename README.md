6eb25dfe10b3c309f7558688124517fb645a5aff
SergSmith64 <sergsmith2008@yandex.ru>
2021-02-07 22:19:55 +0400

    feat: Add Left & Right buttons

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. npm audit fix
 
 2. npm i source-map-loader --save-dev
+
+3. Add Left & Right buttons
--- src/index.js
+++ src/index.js
@@ -6,35 +6,62 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
+const tomLeftBtn = document.getElementById('tomLeft');
+const tomRightBtn = document.getElementById('tomRight');
+
 let state = 0;
+
+let summa = 10;
+let left = 5;
+let right = summa - left;
 
 // ___ оберну в функцию и вызову ее ниже ___
 function render() {
-    // ___ присваиваем счетчику значение ___
-    counter.textContent = state.toString();
-
-    // console.log("__счетчик__ = ", state);
+	// ___ присваиваем счетчику значение ___
+	counter.textContent = state.toString();
+	lll.textContent = left.toString();
+	rrr.textContent = right.toString();
 }
 
 addBtn.addEventListener('click', () => {
-    state++;
-    render();
+	state++;
+	console.log("__state__ = ", state);
+	render();
 });
 
 subBtn.addEventListener('click', () => {
-    state--;
-    render();
+	state--;
+	console.log("__state__ = ", state);
+	render();
 });
 
 asyncBtn.addEventListener('click', () => {
-    setTimeout ( () => {
-        state++;
-        render();
-    }, 2000)
+	setTimeout ( () => {
+		state++;
+		render();
+	}, 2000)
 });
 
 themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+	document.body.classList.toggle('dark');
+});
+
+tomLeftBtn.addEventListener('click', () => {
+	if (left < summa) {
+		left++;
+		right--;
+	  }
+	console.log("__left__ = ", left);
+	render();
+});
+
+tomRightBtn.addEventListener('click', () => {
+	if (right < summa) {
+		left--;
+		right++;
+	  }
+	console.log("__right__ = ", right);
+	render();
 });
 
 render();
